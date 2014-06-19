1e5df1abea9ff8563aec18708fbb5b84a2c87e41
wk1 <wk1>
2014-06-19 14:09:34 +0800

    inti

---
+++ CodeTemplater/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2014/6/19
+ * 时间: 10:09
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace CodeTemplater
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.tbtnRunTemplate = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.txtData = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.txtColumnSpliter = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.toolStrip1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.tbtnRunTemplate,
+									this.toolStripButton2,
+									this.toolStripButton3});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(458, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// tbtnRunTemplate
+			// 
+			this.tbtnRunTemplate.Image = ((System.Drawing.Image)(resources.GetObject("tbtnRunTemplate.Image")));
+			this.tbtnRunTemplate.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.tbtnRunTemplate.Name = "tbtnRunTemplate";
+			this.tbtnRunTemplate.Size = new System.Drawing.Size(76, 22);
+			this.tbtnRunTemplate.Text = "模板替换";
+			this.tbtnRunTemplate.Click += new System.EventHandler(this.TbtnRunTemplateClick);
+			// 
+			// toolStripButton2
+			// 
+			this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+			this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton2.Name = "toolStripButton2";
+			this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton2.Text = "toolStripButton2";
+			// 
+			// toolStripButton3
+			// 
+			this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+			this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton3.Name = "toolStripButton3";
+			this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton3.Text = "toolStripButton3";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Location = new System.Drawing.Point(0, 318);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(458, 22);
+			this.statusStrip1.TabIndex = 2;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.tabControl1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(0, 25);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(458, 293);
+			this.panel2.TabIndex = 4;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(458, 293);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.panel3);
+			this.tabPage1.Controls.Add(this.panel1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(450, 267);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "数据";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.txtData);
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel3.Location = new System.Drawing.Point(3, 48);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(444, 216);
+			this.panel3.TabIndex = 1;
+			// 
+			// txtData
+			// 
+			this.txtData.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtData.Location = new System.Drawing.Point(0, 0);
+			this.txtData.Multiline = true;
+			this.txtData.Name = "txtData";
+			this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtData.Size = new System.Drawing.Size(444, 216);
+			this.txtData.TabIndex = 0;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.txtColumnSpliter);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(3, 3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(444, 45);
+			this.panel1.TabIndex = 0;
+			// 
+			// txtColumnSpliter
+			// 
+			this.txtColumnSpliter.Location = new System.Drawing.Point(97, 10);
+			this.txtColumnSpliter.Name = "txtColumnSpliter";
+			this.txtColumnSpliter.Size = new System.Drawing.Size(69, 21);
+			this.txtColumnSpliter.TabIndex = 1;
+			this.txtColumnSpliter.Text = "\t";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(26, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 21);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "列分隔符";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(450, 267);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "模板代码";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(450, 267);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "替换结果";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(458, 340);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.toolStrip1);
+			this.Name = "MainForm";
+			this.Text = "CodeTemplater";
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox txtData;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtColumnSpliter;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.ToolStripButton toolStripButton3;
+		private System.Windows.Forms.ToolStripButton toolStripButton2;
+		private System.Windows.Forms.ToolStripButton tbtnRunTemplate;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ CodeTemplater/MainForm.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2014/6/19
+ * 时间: 10:09
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace CodeTemplater
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		/// <summary>
+		/// 转换文本数据
+		/// </summary>
+		/// <param name="data"></param>
+		/// <param name="colSep"></param>
+		/// <returns></returns>
+		protected List<List<string>> _parseData(string data, string colSep)
+		{
+			data = data.Replace("\r", "");
+			string[] lines = data.Split("\n");
+			List<List<string>> ret = new List<List<string>>();
+			foreach (string line in lines) {
+				string[] cols = line.Split(colSep);
+				ret.Add(cols.ToList());
+			}
+			return ret;
+		}
+		
+		void TbtnRunTemplateClick(object sender, EventArgs e)
+		{
+			string dataStr = txtData.Text;
+			string colSep = txtColumnSpliter.Text;
+			
+			List<List<string>> rows = _parseData(dataStr, colSep);
+		}
+	}
+}
